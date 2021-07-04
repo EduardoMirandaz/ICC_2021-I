243b1f8c7c2a4826823e3b177a57222a43766a6f
EduardoMirandaz <eduardo.cmazevedo@gmail.com>
2021-07-03 21:02:58 -0300

    correção no exercício 13.2

--- AULA 13/13.2/13,2/13.2.c
+++ AULA 13/13.2/13,2/13.2.c
@@ -1,28 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int vetorDeInt(int n, int vetor);
+/**
+ Crie uma função que recebe um inteiro N como
+argumento, aloca e lê um vetor de N inteiros.
+Crie outra função que recebe um vetor de inteiros
+e o número de elementos no vetor (N) e retorna a
+soma dos positivos.
+O programa principal deve ler o valor de N,
+executar as duas funções, imprimir o valor
+retornado pela 2ª função e liberar a memória
+alocada pela 1ª função.
+
+*/
+int somaPosi(int *pV, int n);
+void vetorDeInt(int n, int **vetor);
+
 int main()
 {
-    int n, *pV, memory;
+    int n, *pV, soma;
     scanf("%d", &n);
-    memory = (int *) calloc(n, sizeof(int));
-    printf("%d", memory);
-}/*
-    int vetor[*pV];
-    vetorDeInt(n, vetor);
+    vetorDeInt(n, &pV);
+    soma = somaPosi(pV, n);
+    printf("%d", soma);
+    free(*pV);
 }
 
-int vetorDeInt(int n, int vetor)
-{
-    int soma, aux = 0;
-    for (int i = 0; i < n; i++){
-        scanf("%d", vetor[i]);
-        if(vetor[i]%2 == 0){
-            soma = vetor[i] + aux;
-            aux = soma;
+void vetorDeInt(int n, int **pV){
+    // dois ponteiros pois quero o ENDEREÇO de um ponteiro!
+    *pV = (int *) calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++)
+        scanf("%d", (*pV+i));
+}
+
+int somaPosi(int *pV, int n){
+    int soma = 0;
+    for(int i = 0; i < n; i++){
+        if(pV[i] > 0)
+        {
+            soma = pV[i] + soma;
         }
     }
-    printf("%d", soma);
+    return soma;
 }
-*/
